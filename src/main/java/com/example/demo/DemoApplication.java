8084279bb0445af3e221084e7512e893a2aef4e1
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-09-05 15:38:31 +0530

    second commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	public static void main(String[] user1) {
+		SpringApplication.run(DemoApplication.class, user1);
 	}
 
 }
